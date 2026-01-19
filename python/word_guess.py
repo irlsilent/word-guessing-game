5fa0e826e112d204d166643b2d8bbb621dfcb68d
Thomas Loonis <117849378+irlsilent@users.noreply.github.com>
2026-01-19 12:20:32 +0000

    Add Python program skeleton and basic game flow

---
+++ python/word_guess.py
@@ -0,0 +1,34 @@
+import random
+
+with open("C:/Users/thoma/Projects/word-guessing-game/data/words.txt") as textFile:
+    words = textFile.readlines()
+random_word = random.choice(words).strip()
+
+name = input("what is your name? ")
+print(f"Good luck! {name}")
+
+print("A 5 letter word has been chosen.")
+print("Guess the characters: ")
+
+guesses = ''
+turns = 12
+
+while turns > 0:
+    failed = 0
+    for char in random_word:
+        if char in guesses:
+            break
+        else:
+            break
+    
+    if failed == 0:
+        break
+    
+    print()
+    guess = input("Guess a character: ")
+    
+    guesses += guess
+    
+    if guess not in random_word:
+        break
+    
