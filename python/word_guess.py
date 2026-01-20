ea716a586c66f0ac4b1dd964be1f23ee43db28c1
Thomas Loonis <117849378+irlsilent@users.noreply.github.com>
2026-01-20 11:25:59 +0000

    Add core letter guessing logic and feedback

--- python/word_guess.py
+++ python/word_guess.py
@@ -7,7 +7,7 @@ random_word = random.choice(words).strip()
 name = input("what is your name? ")
 print(f"Good luck! {name}")
 
-print("A 5 letter word has been chosen.")
+print("A 5 letter word has been chosen. Use Capitals in order to guess.")
 print("Guess the characters: ")
 
 guesses = ''
@@ -17,13 +17,17 @@ while turns > 0:
     failed = 0
     
     for char in random_word:
-        print("_", end=" ")
-        if char in guesses:
-            break
         
+        if char in guesses:
+            print(char, end=" ")
+        else:
+            print("_", end=" ")
+            failed +=1
         
     
     if failed == 0:
+        print(f"Congratulations {name}. You win!")
+        print(f"The word was {random_word}.")
         break
     
     print()
@@ -32,5 +36,11 @@ while turns > 0:
     guesses += guess
     
     if guess not in random_word:
-        break
+        turns -=1
+        print("Wrong!")
+        print(f"You have {turns} more guesses.")
+        
+        if turns == 0:
+            print("Oh no! You lost!")
+            print(f"The word was {random_word}")
     
