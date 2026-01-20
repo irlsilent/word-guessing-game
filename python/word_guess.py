24fa3bbbee6765445b2303d2889ced7b0b0875fd
Thomas Loonis <117849378+irlsilent@users.noreply.github.com>
2026-01-20 11:32:19 +0000

    Improve input validation and handle duplicate guesses

--- python/word_guess.py
+++ python/word_guess.py
@@ -4,7 +4,7 @@ with open("C:/Users/thoma/Projects/word-guessing-game/data/words.txt") as textFi
     words = textFile.readlines()
 random_word = random.choice(words).strip()
 
-name = input("what is your name? ")
+name = input("What is your name? ")
 print(f"Good luck! {name}")
 
 print("A 5 letter word has been chosen. Use Capitals in order to guess.")
@@ -15,6 +15,8 @@ turns = 12
 
 while turns > 0:
     failed = 0
+    
+
     
     for char in random_word:
         
@@ -33,9 +35,18 @@ while turns > 0:
     print()
     guess = input("Guess a character: ")
     
-    guesses += guess
     
-    if guess not in random_word:
+    # Validation of the guess
+    if not guess.isalpha():
+        print('Enter only a LETTER')
+        continue
+    elif len(guess) > 6:
+        print('Enter only a FIVE letter word')
+        continue
+    elif guess in guesses:
+        print('You have already guessed that letter')
+        continue
+    elif guess not in random_word:
         turns -=1
         print("Wrong!")
         print(f"You have {turns} more guesses.")
@@ -43,4 +54,5 @@ while turns > 0:
         if turns == 0:
             print("Oh no! You lost!")
             print(f"The word was {random_word}")
-    
+        continue
+    guesses += guess
